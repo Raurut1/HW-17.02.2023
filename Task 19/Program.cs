f71d6534c04fe1dbbec3a8671e03629ecd5e7aeb
Raurut <makey@yandex.ru>
2023-02-17 12:07:05 +0300

    TAsk 19 complete

---
+++ Task 19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - Не пятизначное число!");
+}
